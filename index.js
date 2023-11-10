a9fde4cc5d0c01fc18d286cc0d24dd6ac27d79b4
r4vemode <SocnovkAA@yandex.ru>
2023-11-10 22:53:11 +0300

    q
    q

---
+++ index.js
@@ -0,0 +1,11 @@
+const mySwiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: {
+      delay: 2000,
+  },
+  speed: 800,
+  effect: 'slide',
+  slidesPerView: 4, 
+  spaceBetween: 40, 
+
+});
